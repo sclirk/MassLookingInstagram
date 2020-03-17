f17849e0b31ceddb237f4cac3da2e07e1d12ed3f
sclirk <54840390+sclirk@users.noreply.github.com>
2020-03-17 16:13:45 +0300

    Update clsk.py

--- clsk.py
+++ clsk.py
@@ -1,132 +1,59 @@
-from selenium import webdriver
-from time import sleep
+from instalook import *
+
+look = looker("USERNAME", "FIND_PROFILE_NAME", "PASS_YOUR_PROFILE", "example_location_for_webdriver(C:\chromedriver.exe)")
+look.open_insta()
 
 
-def looker():
-    global name
-    name = "marizhelby"
-    child = 0
-    iteration = 10
-    default_name = name
-    browser = webdriver.Chrome("C:/chromedriver.exe")
-    browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+class global_looker:
+    def __init__(self, iteration):
+        self.iteration = iteration
 
-    def max_sleep():
-        sleep(25)
-
-    def long_sleep():
-        sleep(5)
-
-    def mid_sleep():
-        sleep(3)
-
-    def short_sleep():
-        sleep(1)
-
-    def name_user_and_log_in():
-        browser.find_element_by_name("username").send_keys('passw')
-        browser.find_element_by_name('password').send_keys('passw')
-        browser.find_element_by_css_selector('#react-root > section > main > div > article > div > '
-                                             'div:nth-child(1) > div > form '
-                                             '> div:nth-child(4) > button > '
-                                             'div').click()
-        max_sleep()
-
-        browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div['
-                                      '2]/button').click()
-        long_sleep()
-
-        browser.find_element_by_css_selector(
-            'body > div.RnEpo.Yx5HN > div > div > div.mt3GC > button.aOOlW.HoLwm').click()
-        mid_sleep()
-
-    long_sleep()
-    name_user_and_log_in()
-    browser.find_element_by_class_name('TqC_a').click()
-    short_sleep()
-
-    browser.find_element_by_css_selector(
-        '#react-root > section > nav > div._8MQSO.Cx7Bp '
-        '> div > div > div.LWmhU._0aCwM > input').send_keys('{}'.format(name))
-    mid_sleep()
-    browser.find_element_by_class_name('Ap253').click()
-    mid_sleep()
-
-    browser.find_element_by_css_selector(
-        '#react-root > section > main > div > header > section > ul > li:nth-child(2) > a').click()
-    mid_sleep()
-
-    def find_name():
-        browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div').click()
-        mid_sleep()
-        browser.find_element_by_css_selector(
-            '#react-root > section > nav > '
-            'div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > input').send_keys('{}'.format(name))
-        mid_sleep()
-        browser.find_element_by_class_name('Ap253').click()
-        mid_sleep()
-        browser.find_element_by_css_selector(
-            '#react-root > section > main > div > header > section > ul > li:nth-child(2) > a').click()
-        mid_sleep()
-
-    def click_followers():
-        short_sleep()
-        browser.find_element_by_xpath(
-            '/html/body/div[4]/div/div[2]/ul/div/li[{}]/div/div[1]/div'.format(child)).click()
-        short_sleep()
-
-    def another_click_followers():
-        short_sleep()
-        browser.find_element_by_xpath(
-            '/html/body/div[3]/div/div[2]/ul/div/li[{}]/div/div[1]/div[1]'.format(child)).click()
-        long_sleep()
-
-    while True:
-        print(child, iteration)
-        child += 1
-        if child < iteration:
-            try:
-                short_sleep()
-                click_followers()
-            except Exception:
-                another_click_followers()
-            try:
-                short_sleep()
-                find_name()
-            except Exception:
+    def cycle(self):
+        while True:
+            look.child += 1
+            if look.child < self.iteration:
                 try:
-                    browser.find_element_by_css_selector(
-                        '#react-root > section > div > div > section > div.GHEPc > button.-jHC6').click()
-                    short_sleep()
+                    look.short_sleep()
+                    look.click_followers_with_params(3)
                 except Exception:
-                    find_name()
-        else:
-            click_followers()
-            try:  # have story
-                story_close = browser.find_element_by_css_selector(
-                    '#react-root > section > div > div > section > header > div > div.MS2JH > div > div > div > a')
-                story_close.click()
-                short_sleep()
-                iteration = 10
-                browser.find_element_by_css_selector(
-                    '#react-root > section > div > div >'
-                    ' section > header > div > div.MS2JH > div > div > div > a').click()
-                short_sleep()
-                history = browser.find_element_by_css_selector(
-                    "#react-root > section > main > div > header > section > div.nZSzR > h1")
-                name = history.text
-                child = 0
-                long_sleep()
-                browser.find_element_by_xpath(
-                    '//*[@id="react-root"]/section/nav/div[2]/div/div/div[1]/a/div').click()
-                find_name()
-            except Exception:  # without story
-                profile = browser.find_element_by_css_selector(
-                    "#react-root > section > main > div > header > section > div.nZSzR > h1")
-                name = profile.text
-                short_sleep()
-                iteration -= 1
-                find_name()
+                    look.click_followers_with_params(4)
+                try:
+                    look.short_sleep()
+                    look.find_name()
+                except Exception:
+                    try:
+                        look.browser.find_element_by_css_selector(
+                            '#react-root > section > div > div > section > div.GHEPc > button.-jHC6').click()
+                        look.short_sleep()
+                    except Exception:
+                        look.find_name()
+            else:
+                look.click_followers_with_params(4)
+                try:
+                    story_close = look.browser.find_element_by_css_selector(
+                        '#react-root > section > div > div > section > header > div > div.MS2JH > div > div > div > a')
+                    story_close.click()
+                    self.iteration = 10
+                    look.browser.find_element_by_css_selector(
+                        '#react-root > section > div > div >'
+                        ' section > header > div > div.MS2JH > div > div > div > a').click()
+                    look.short_sleep()
+                    history = look.browser.find_element_by_css_selector(
+                        "#react-root > section > main > div > header > section > div.nZSzR > h1")
+                    look.name = history.text
+                    look.child = 0
+                    look.long_sleep()
+                    look.browser.find_element_by_xpath(
+                        '//*[@id="react-root"]/section/nav/div[2]/div/div/div[1]/a/div').click()
+                    look.find_name()
+                except Exception:
+                    profile = look.browser.find_element_by_css_selector(
+                        "#react-root > section > main > div > header > section > div.nZSzR > h1")
+                    look.name = profile.text
+                    look.short_sleep()
+                    self.iteration -= 1
+                    look.find_name()
 
 
-looker()
+gl = global_looker(10)
+gl.cycle()
